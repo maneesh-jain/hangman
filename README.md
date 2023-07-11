47523ade90c013e0e9753353c3ad422f8a9b26cb
unknown <maneeshj@yahoo.com>
2023-07-11 10:52:42 +0100

    Milestone 4 complete

--- README.md
+++ README.md
@@ -9,11 +9,25 @@ Set up Git and GitHub.
 
 ## Milestone 2
 
-Built a list of 5 words and chosen a random word for the user to try and guess.
+Built a *list* of 5 words and chose a random word for the user to try and guess.
 Updated this Readme file with Milestones 1 & 2.
 
 ## Milestone 3
 
-Defined two separate functions to aid code readability: 
+Defined two separate *functions* to aid code readability: 
 - ask_for_input - to request input from the user and check it's a valid letter of the alphabet; this largely reused code from Milestone 2 
 - check_guess - to see if the user's letter is in the word to be guessed
+
+## Milestone 4
+
+Created a Hangman *class* with the following *attributes*:
+- word_list - list of words for computer to choose from
+- num_lives - no. of lives user has
+- word - word randomly chosen by computer
+- word_guessed - word guessed by user
+- num_letters - no. of unique letters left to guess
+- list_of_guesses - letters guessed
+
+The two existing functions were coded as *methods* and extended:
+- ask_for_input - now checks if the user has already guessed that letter
+- check_guess - now updates *word_guessed* attribute and deducts a life if the guess is wrong
---
+++ milestone4.py
@@ -0,0 +1,44 @@
+import random
+
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list # list of words for computer to choose from
+        self.num_lives = num_lives # no. of lives user has
+        self.word = random.choice(self.word_list) # word randomly chosen by computer
+        print(self.word) # checker
+        self.word_guessed = list('_' * len(self.word)) # word guessed by user
+        # no. of unique letters left to guess:
+        self.num_letters = len(self.word) - len(set(self.word_guessed)) 
+        self.list_of_guesses = [] # letters guessed
+    
+    def ask_for_input(self):
+        while True:
+            guess = input('Guess a letter: ')
+            if not(len(guess) == 1 and guess.isalpha()):
+                print('Invalid letter. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word.')
+
+            for i in range(len(self.word)):
+                if guess == self.word[i]:
+                    self.word_guessed[i] = guess
+            print(self.word_guessed) # checker
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives left.')
+        return guess
+
+h = Hangman(['apple', 'banana', 'orange', 'cherry', 'mango'])
+h.ask_for_input()
